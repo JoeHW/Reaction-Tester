9b0027550520f8889e7b705fe23bf163bb1ccd94
Joe Williamson <jhwilliamson@outlook.com>
2017-08-27 17:43:09 -0400

    Markup

---
+++ ReactionTest.js
@@ -0,0 +1,25 @@
+//access the pixel border and put a class of shape inside of it
+//random calcs depend on preset shape sizex
+//determine max and minimum shape sizes
+//onclick function sets off random style setter
+
+
+
+
+
+//onclick = () => function reset:
+clear html
+let newshape = document.createElement('A'):
+newshape.style.margin = func;
+newshape.style.size = func;
+newshape.style.borderRadius = func;
+append to correct div etc
+
+timer toggle Flip boolena?
+
+
+
+
+
+//var element = document.createElement('select');
+//element.style.width = "100px";
