3043d55d3b209ed131481a67c974ed3aeb4cbbeb
Joe Williamson <jhwilliamson@outlook.com>
2017-08-28 14:53:35 -0400

    refactor

--- ReactionTest.js
+++ ReactionTest.js
@@ -1,6 +1,5 @@
 
-let timerToggle = false;
-let needShape = true;
+let shapeAfterDelay = () => setTimeout(shapeGenerator, Math.random() * 2000);
 
 let randomSize = (min, max) => {
   min = Math.ceil(min);
@@ -9,43 +8,29 @@ let randomSize = (min, max) => {
 };
 
 let colorArray = ['red', 'blue', 'green', 'yellow', 'orange'];
-let shapePicker = ["50%", "null"];
+let shapePicker = ["0", "50%"];
 
 let shapeGenerator = () => {
-let shapeBox = document.getElementById('pixelborder');
-let newShape = document.createElement("div");
-newShape.id = "shape";
+let newShape = document.getElementById('pixelborder');
+newShape.style.display = "block";
 newShape.style.width = "" + randomSize(50,350) + "px";
 newShape.style.height = newShape.style.width;
 newShape.style.backgroundColor = colorArray[randomSize(0,4)];
-newShape.style.borderRadius = shapePicker[randomSize(0,1)];
+newShape.style.borderRadius = shapePicker[randomSize(0,4)];
 newShape.style.marginTop = "" + randomSize(50,300) + "px";
 newShape.style.marginBottom = "" + randomSize(50,300) + "px";
 newShape.style.marginRight = "" + randomSize(50,300) + "px";
 newShape.style.marginLeft = "" + randomSize(50,300) + "px";
-shapeBox.appendChild(newShape);
-//document.body.insertBefore(newShape, shapeBox);
-timerToggle = true;
 let start = new Date().getTime();
 
-let clickedShape = document.getElementById("shape");
-clickedShape.addEventListener('click', () => {
-clickedShape.parentNode.removeChild(clickedShape);
+let clickedShape = document.getElementById("pixelborder");
+clickedShape.onclick = () => {
+clickedShape.style.display = "none";
 let end = new Date().getTime();
 let timeTaken =  (end - start)/1000;
 document.getElementById("timer").innerHTML = timeTaken;
-timerToggle = false;
-needShape = true;
- });
+shapeAfterDelay();
+ };
 };
 
-const newGenerator = () => {
-  let begin = () => {setInterval(shapeGenerator, randomSize(2000, 5000))}
-  if(!needShape){return;}
-  else {
-  needShape = false;
-  begin();
-}};
-
-
-document.body.onload = newGenerator();
+document.body.onload = shapeGenerator();
