6cbae2f135117042123e87255d7cb1ca8485c1ad
Joe Williamson <jhwilliamson@outlook.com>
2017-08-28 12:11:38 -0400

    Timer and shapes working

--- ReactionTest.js
+++ ReactionTest.js
@@ -35,7 +35,16 @@
 // <div id="timer"></div>
 // <div id ="stop_timer" onclick="clearInterval(timerVar)">Stop time</div>
 
-
+let timerVar;
+const time = () => {
+  if(timerToggle){
+  timerVar = setInterval(countTimer, 1000);
+} else {
+  clearInterval(timerVar);
+  totalSeconds = 0
+  document.getElementById("timer").innerHTML = totalSeconds;
+}
+}
 
 let totalSeconds = 0;
 
@@ -44,9 +53,10 @@ let countTimer = () => {
 document.getElementById("timer").innerHTML = totalSeconds
 }
 
-let timerToggle = false;
-let timerVar = setInterval(countTimer, 1000);
 
+
+let timerToggle = false;
+let needShape = true
 
 let randomSize = (min, max) => {
   min = Math.ceil(min);
@@ -60,11 +70,49 @@ let shapePicker = ["50%", "null"];
 let shapeGenerator = () => {
 let shapeBox = document.getElementById('pixelborder');
 let newShape = document.createElement("div");
+newShape.id = "shape";
 newShape.style.width = "" + randomSize(50,350) + "px";
 newShape.style.height = newShape.style.width;
 newShape.style.backgroundColor = colorArray[randomSize(0,4)];
 newShape.style.borderRadius = shapePicker[randomSize(0,1)];
 document.body.insertBefore(newShape, shapeBox);
+timerToggle = true;
+time();
+
+let clickedShape = document.getElementById("shape");
+clickedShape.addEventListener('click', () => {
+clickedShape.parentNode.removeChild(clickedShape);
+timerToggle = false;
+time();
+needShape = true;
+});
+
 };
 
-document.body.onload = shapeGenerator();
+// var element = document.getElementById("element-id");
+// element.parentNode.removeChild(element);
+
+// clickedShape[0].addEventListener('click', (event) => {
+// event.target.style.display = "none"
+// timerToggle = false;
+// time();
+// });
+
+//})
+
+//document.body.onload = shapeGenerator();
+
+const newGenerator = () => {
+  let start;
+  if(needShape){
+  start = setInterval(shapeGenerator, randomSize(2000, 5000));
+  needShape = false;
+}}
+
+// let rolling = () =>{
+// if(!timerToggle){newGenerator();}
+// }
+
+document.body.onload = newGenerator();
+
+//rolling();
